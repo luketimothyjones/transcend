10996b782605ed41fac76941fd5f0b9c3d6c7d52
Luke Jones <underdweller@gmail.com>
2019-05-01 15:34:50 -0700

    Make config open command a relative path

--- Unity/Source/UX-scripts/open-config.bat
+++ Unity/Source/UX-scripts/open-config.bat
@@ -1,1 +1,1 @@
-START notepad "D:\Google Drive\Classes\Senior Project\Workspace\Transcend\Unity\Build\transcend_demo_Data\transcend-broker\config\devices.cfg"
+START notepad ".\transcend_demo_Data\transcend-broker\config\devices.cfg"
